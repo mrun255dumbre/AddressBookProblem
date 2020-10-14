e34fb4ddfc0d09e06d846b44c721f4fb72207d96
mrun255dumbre <mrun255dumbre@gmail.com>
2020-10-14 19:24:26 +0530

    Method implemented for edit record

--- src/addressBook/AddressBookImplementation.java
+++ src/addressBook/AddressBookImplementation.java
@@ -1,5 +1,10 @@
 package addressBook;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -7,34 +12,38 @@ import java.util.Scanner;
 
 public class AddressBookImplementation implements AddressBookInterface{
 	String firstName,lastName,address,city,state,zip,phoneNumber;
+	int flag = 0;
+	public static File file;
+	BufferedReader br;
+	BufferedWriter bw;
 	ArrayList<Person> addressBookArrayList=new ArrayList<Person>();
 	Scanner sc = new Scanner(System.in);
 	@Override
 	public void addPerson() {
 		String new_line="\n";
 		String comma=",";
-		
+
 		try {
 			FileWriter fileWriter = new FileWriter("addressBook.csv");
 	        fileWriter.append("FirstName,LastName,Address,State,City,Zip,PhoneNumber");
 			System.out.println("Enter First Name : ");
 	    	firstName=sc.next();
-	    	
+
 	    	System.out.println("Enter Last Name : ");
 	    	lastName=sc.next();
-	    	
+
 	    	System.out.println("Enter Address : ");
 	    	address=sc.next();
-	    	
+
 	    	System.out.println("Enter City : ");
 	    	city=sc.next();
-	    	
+
 	    	System.out.println("Enter State : ");
 	    	state=sc.next();
-	    	
+
 	    	System.out.println("Enter Zipcode : ");
 	    	zip=sc.next();
-	    	
+
 	    	System.out.println("Enter Phone Number : ");
 	    	phoneNumber=sc.next();
 	    	addressBookArrayList.add(new Person(firstName,lastName,address,city,state,zip,phoneNumber));
@@ -61,6 +70,89 @@ public class AddressBookImplementation implements AddressBookInterface{
         catch(IOException e) {
         	e.printStackTrace();
         }
+	}
+	@Override
+	public void editPerson(String fileName) {
+		System.out.println("Enter phone number for edit person data\n");
+		String lineToFind = sc.next();
+		String line = null;
+		File inFile = new File((fileName+".csv"));
+		File tempFile = new File("temp.csv");
+		
+		try {
+			br = new BufferedReader(new FileReader(inFile));
+			bw = new BufferedWriter(new FileWriter(tempFile));
+			
+			while ((line = br.readLine()) != null) {
+				if (line.trim().contains(lineToFind)) {
+					
+					System.out.println("Record for given phone number\n" + line);
+					
+					String[] persondetails = line.split(",");
+					
+					String firstname = persondetails[0];
+					String lastname = persondetails[1];
+					
+					System.out.println("enter the address");
+					String address = sc.next();
+					
+					System.out.println("enter the city");
+					String city = sc.next();
+					
+					System.out.println("enter the State");
+					String state = sc.next();
+					
+					System.out.println("enter the Zipcode");
+					int zipcode = sc.nextInt();
+					
+					String phonenumber = persondetails[6];
+					
+					bw.write(firstname);
+					bw.write("," + lastname);
+					bw.write("," + address);
+					bw.write("," + city);
+					bw.write("," + state);
+					bw.write("," + zipcode);
+					bw.write("," + phonenumber);
+					bw.newLine();
+					flag++;
+				} 
+				else {
+					bw.write(line);
+					bw.newLine();
+				}
+			}
+			bw.close();
+			br.close();
+			
+			inFile.delete();
+			tempFile.renameTo(inFile);
+			
+			if (flag == 0)
+				System.out.println("Record found in AddressBook :" + fileName);
+			else
+				System.out.println("Record Edited Successfully");
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	@Override
+	public void display(String fileName) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Records Present in System :");
+		try {
+			scanner = new Scanner(new File(fileName+".csv"));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		while (scanner.hasNextLine()) {
+			String line = scanner.nextLine();
+			System.out.println(line);
+		}
 	}
 	
 }
--- src/addressBook/AddressBookInterface.java
+++ src/addressBook/AddressBookInterface.java
@@ -2,4 +2,6 @@ package addressBook;
 
 public interface AddressBookInterface {
 	public void addPerson();
+	public void editPerson(String fileName);
+	public void display(String fileName);
 }
